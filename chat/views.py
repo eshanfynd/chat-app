d8b66aa92e22b4293689298284e42d5f07697da5
Eshan Pandey <eshanpandey@MacBook-Pro-35.local>
2024-10-10 15:41:10 +0530

    changes

--- chat/views.py
+++ chat/views.py
@@ -6,28 +6,6 @@ websocket_mapping = {}
 
 
 async def chat(mobile_no: int, websocket: WebSocket):
-    await websocket.accept()
-    user_sockets = websocket_mapping.get(mobile_no, [])
-    user_sockets.append(websocket)
-    websocket_mapping.update({
-        mobile_no: user_sockets
-    })
-    while True:
-        data = await websocket.receive_text()
-        try:
-            print(data)
-            # message_details = {"to": 8850255428}
-            message_details = json.loads(data)
-            receivers = websocket_mapping.get(message_details.get("to"))
-            print(receivers)
-            if receivers:
-                for receiver in receivers:
-                    try:
-                        text = message_details.get("text")
-                        await receiver.send_text(f"{text}")
-                    except Exception as receiverError:
-                        print(str(receiverError))
-                    # await receiver.send_text(f"{text} received from {mobile_no}")
-        except Exception as e:
-            print(e)
-        # await websocket.send_text(f"acknowledge to {mobile_no}")
+    API_KEY = "aooeoq293u120412i4124eo0o12ro123o--1239"
+    pass
+    # warning, this code should not be pushed as this is.
